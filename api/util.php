ad28a7f775159fe99160ae19abe8318935760430
Nathan Baines <nathan.baines@nathcat.net>
2026-01-15 19:47:01 +0000

    Corrected table name in is member check

--- api/util.php
+++ api/util.php
@@ -1,13 +1,13 @@
 <?php
 function is_member_of_group($conn, $group, $user)
 {
-    $stmt = $conn->prepare("SELECT count(*) AS 'count' FROM `Groups` WHERE owner = ? AND id = ?");
+    $stmt = $conn->prepare("SELECT count(*) AS 'count' FROM DataCat.`Groups` WHERE owner = ? AND id = ?");
     $stmt->bind_param("ii", $user, $group);
     $stmt->execute();
 
     $res = $stmt->get_result()->fetch_assoc()["count"];
     if ($res != 1) {
-        $stmt = $conn->prepare("SELECT count(*) AS 'count' FROM `Group_Members` WHERE `group` = ? AND `user` = ?");
+        $stmt = $conn->prepare("SELECT count(*) AS 'count' FROM DataCat.`Group_Members` WHERE `group` = ? AND `user` = ?");
         $stmt->bind_param("ii", $group, $user);
         $stmt->execute();
 
